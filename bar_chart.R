60bf8f4af36368c21fe8edb422c07622bdfb6a7b
Angelina Lum <angelinalum@Angelinas-MBP.hsd1.wa.comcast.net>
2020-05-22 10:30:07 -0700

    cleaned up function

--- bar_chart.R
+++ bar_chart.R
@@ -6,26 +6,6 @@ library("forcats")
 literacy_data <-
   read.csv("data/regional-agg_LiteracyRate.csv", stringsAsFactors = FALSE)
 
-bar_chart <- function(data_frame) {
-  plot_data <- melt(data_frame[, c('Country', 'Male', 'Female')], id.vars = 1)
-  plot_data <- na.omit(plot_data) %>%
-    group_by(Country) %>%
-    select(Country, variable, value)
-  bar_chart <- ggplot(plot_data, aes(x = value, y = Country)) +
-    geom_bar(aes(fill = variable), stat = "identity", position = "dodge") +
-    ggtitle("Literacy Rates of Youth Males and Females in 2018") +
-    labs(y = "Country", x = "Literacy Rate (%)") +
-    labs(fill = "Gender")
-  return(bar_chart)
-}
-
-bar_chart(literacy_data)
-
-# Function
-
-literacy_data <-
-  read.csv("data/regional-agg_LiteracyRate.csv", stringsAsFactors = FALSE)
-
 rates_by_region <- function(data_frame) {
   plot_data <- na.omit(data_frame) %>%
     group_by(Region) %>%
@@ -47,5 +27,3 @@ rates_by_region <- function(data_frame) {
     labs(fill = "Gender")
   return(bar_chart)
 }
-
-rates_by_region(literacy_data)
