9796a8701a700bdbe8e70ea37f8eede2f1efc3c2
Hieu Tran <HieuTran@Hieu-Tran-2.local>
2020-05-22 17:36:50 -0700

    add map chart

--- scripts/map_chart.R
+++ scripts/map_chart.R
@@ -1,5 +1,33 @@
-data <- read.csv("../data/literacy_rate_and_gdp.csv")
+library(dplyr)
+library(leaflet)
 
 map_chart <- function(data) {
+  plot_data <- data %>% 
+    select(
+      Country,
+      Total,
+      latitude,
+      longitude
+    ) %>%
+    filter(
+      !is.na(Total),
+      !is.na(latitude)
+    ) 
   
+  plot_data$radius <- (plot_data$Total - min(plot_data$Total)) / 5
+
+  map <- leaflet(data) %>%
+    addTiles() %>%
+    addCircleMarkers(
+      lat = ~latitude,
+      lng = ~longitude,
+      popup = paste0(
+        "Country: ", data$Country, "<br>",
+        "Literacy rate: ", data$Total, "<br>"
+      ),
+      stroke = FALSE,
+      radius = ~radius,
+      fillOpacity = 0.5
+    )
 }
+
