04fbf6781c57f97e85efc136e0ec7340da42ffa8
Katie Broughton <katieb17@uw.edu>
2020-05-22 18:18:58 -0700

    added scatter plot and table analyses

--- scripts/summary_info.R
+++ scripts/summary_info.R
@@ -4,24 +4,19 @@ sum_inform <- function(data) {
   info <- list()
   info$num_countries <- data %>%
     nrow()
-  
   info$country_highest_total_rate <- data %>%
     filter(Total == max(Total, na.rm = TRUE)) %>%
     pull(Country)
-  
   info$country_lowest_total_rate <- data %>%
     filter(Total == min(Total, na.rm = TRUE)) %>%
     pull(Country)
-  
   info$lowest_total_rate <- data %>%
     filter(Total == min(Total, na.rm = TRUE)) %>%
     pull(Total)
-  
   info$avg_total_rate <- data %>%
     na.omit() %>%
     select(Total) %>%
     summarise(avg = mean(Total))
-    
   info$num_least_developed_countries <- data %>%
     filter(Least.developed.countries..LDC. == "LDC") %>%
     count()
