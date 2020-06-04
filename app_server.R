f0d73266ffab67da9f4153a7ac6931d5ad813b13
Katie Broughton <katieb17@uw.edu>
2020-06-03 18:41:43 -0700

    start barchart added side panel

--- app_server.R
+++ app_server.R
@@ -3,6 +3,7 @@ library("plotly")
 
 # Read in data
 source("scripts/scatter_plot.R")
+source("scripts/bar_chart.R")
 data <- read.csv("data/regional-agg_LiteracyRate.csv", 
                  stringsAsFactors = FALSE)
 
@@ -10,5 +11,9 @@ server <- function(input, output) {
   # Rendered plotly object that returns scatter plot
   output$scatter <- renderPlotly({
     return(scatter_plot(data, input$y_var))
+  })
+  # Renders barchart
+  output$barchart <- renderPlotly({
+    return(bar_chart(data, input$region))
   })
 }
--- app_ui.R
+++ app_ui.R
@@ -19,7 +19,7 @@ scatter_sidebar_content <- sidebarPanel(
   h4("Our Reasoning:"),
   p("In this chart, we wanted to see if there was a similar relationship
     between the least developing countries (LDC) and their overall youth
-    literacy rates. We thought it’d be best to plot this on a scatter plot
+    literacy rates. We thought it'd be best to plot this on a scatter plot
     so we could clearly interpret whether the literacy rates of the youth
     were generally high or low.")
 )
@@ -39,11 +39,42 @@ scatter_panel <- tabPanel(
   )
 )
 
+
+# Widget to pick gender and region
+barchart_sidebar <- sidebarPanel(
+  selectInput(
+    "region",
+    label = "Region:",
+    choices = list(
+      "Sub-Saharan Africa" = "SSA",
+      "South Africa" = "SA",
+      "Middle East and North Africa" = "MENA",
+      "Latin America and the Carribean" = "LAC",
+      "Europe and Central Asia" = "ECA",
+      "East Asia and Pacific" = "EAP"
+    )
+  )
+)
+
+# Prints barchart
+barchart_main_content <- mainPanel(
+  plotlyOutput(outputId = "barchart")
+)
+
+# Puts barchart page together
+barchart_panel <- tabPanel(
+  "Barchart",
+  sidebarLayout(
+    barchart_sidebar,
+    barchart_main_content
+  )
+)
+
 # Defines the 5 pages 
 ui <- navbarPage(
   "Literacy Rates Around the World",
   # overview, 
-  # barchart_panel,
+  barchart_panel,
   scatter_panel
   # map_panel,
   # summary
