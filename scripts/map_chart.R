5a2b7906a963a11b74f83c7adf147726351aad76
Hieu Tran <HieuTran@Hieu-Tran-2.local>
2020-06-04 20:00:32 -0700

    changes in map-chart

--- scripts/map_chart.R
+++ scripts/map_chart.R
@@ -5,26 +5,42 @@ map_chart <- function(data) {
   plot_data <- data %>%
     select(
       Country,
-      Total,
+      selected = "Total",
+      Total.population,
       latitude,
       longitude
     ) %>%
     na.omit()
-
-  plot_data$radius <- plot_data$Total / 15
-                       
-  map <- leaflet(plot_data) %>%
+  
+  plot_data$literacy_level <- cut(
+    plot_data$selected, 
+    c(0, 60, 80, 90, 95, 98.9, 99, 100),
+    include.lowest = T,
+    labels = c("<60%",'60-80%', '80-90%', '90-95%', '>95%', '99%','100%')
+  )
+  
+  factpal <- colorFactor("RdYlGn", plot_data$literacy_level)
+  
+  leaflet(plot_data) %>%
     addTiles() %>%
     addCircleMarkers(
       lat = ~latitude,
       lng = ~longitude,
       popup = paste0(
         "Country: ", plot_data$Country, "<br>",
-        "Literacy rate: ", plot_data$Total, "<br>"
+        "Literacy rate: ", plot_data$selected, "%<br>"
       ),
       stroke = FALSE,
-      color = plot_data$Total,
-      fillOpacity = 0.5
+      color = ~factpal(literacy_level),
+      radius = 8,
+      fillOpacity = 0.8
+    ) %>%
+    addLegend(
+      'bottomright',
+      pal = factpal,
+      values = plot_data$literacy_level,
+      title = 'The literacy rate',
+       opacity = 1
     )
   return(map)
 }
